9a6dc4e30be4504a4c2d52a2ccb76d310d46d8a6
bnickle0000 <bnickle0000@gmail.com>
2023-02-15 02:49:11 -0800

    Interesting bug fix

--- data.py
+++ data.py
@@ -6,24 +6,21 @@ import os
 json documentation: https://docs.python.org/3/library/json.html
 """
 
-"""
-Singleton representing the DM's Discord ID
-"""
-class DMID(object):
-    _singleton = None
-    value = None
+class DMID:
+    def __init__(self, value:str = None):
+        self.__value = None
+        self.setValue(value)
+    def getValue(self):
+        return self.__value
+    def setValue(self, value:str = None):
+        if self.__value == None:
+            self.__value = value
 
-    def __new__(cls, value):
-        if not cls._singleton:
-            cls._singleton = super(DMID, cls).__new__(cls)
-            cls.value = value
-        return cls._singleton
-
-# dmid1 = DMID("id-01")
+"""# dmid1 = DMID("id-01")
 # # dmid1.shared = "id-01"
 # print(dmid1.value)
 # dmid1 = DMID("id-02")
-# print(dmid1.value)
+# print(dmid1.value)"""
 
 """
 Represents the D&D player. Allows for a player to be in many servers
@@ -55,6 +52,7 @@ class Player:
         self.HOUR = "hoursRemaining"
         self.STAT = "status"
         self.PID = "pid"
+        self.TYPE = "type"
 
     def setPid(self, pid:str):
         self.pid = pid
@@ -94,6 +92,7 @@ class Player:
         type[description][self.DURA] = largerDuration
         type[description][self.HOUR] = largerRemaining
         type[description][self.STAT] = config.effectStatus[config.E_ACTIVE]
+        type[description][self.TYPE] = effectType
         return 0
     def cureEffect(self,
                    description:str # May not uniquely id effect instances, but good enough
@@ -130,6 +129,34 @@ class Player:
                 else:
                     result[curr[self.DESC]] = curr
         return result
+    # Return a human-readable list of effects the player is experiencing
+    # Should look something like
+    """
+    {Player} has the effect:
+    > {description}
+    > Type: {type}
+    > Status: {status}
+    > Max duration: {duration}
+    > Hours remaining: {hoursRemaining}
+    """
+    def returnPrintable(self, name):
+        lines = []
+        fx = []
+        for effectType in self.allEffects.keys():
+            effectSet = self.allEffects[effectType]
+            fx = fx + [effectSet[key] for key in effectSet.keys()]
+        for effect in fx:
+            message = "\n".join([f"{name} has the following effect:",
+                                 f"--------------------------------",
+                                 f"> {effect[self.DESC]}",
+                                 f"--------------------------------",
+                                 f"> Type: {effect[self.TYPE]}",
+                                 f"> Status: {effect[self.STAT]}",
+                                 f"> Max duration: {effect[self.DURA]}",
+                                 f"> Hours remaining: {effect[self.HOUR]}"])
+            lines.append(message)
+
+        return lines
     def toJson(self):
         result = self.allEffects
         result[self.PID] = self.pid
@@ -153,8 +180,8 @@ class Server:
     def addPlayer(self, player:Player):
         pid = player.getPid()
         self.players[pid] = player
-    def getDMID(self):
-        return self.dmid.value
+    def getDMID(self) -> str:
+        return self.dmid.getValue()
     def cureEffect(self,pid:str,effect:str):
         player = self.players[pid]
         player.cureEffect(effect)
@@ -168,13 +195,21 @@ class Server:
     def toJson(self):
         result = dict()
         result[self.SID] = self.sid
-        result[self.DMID] = self.dmid.value
+
+        """
+        WEIRD BUG I have NO idea why the commented out line below doesn't work
+        but then the one directly below it 
+        """
+        # result[self.DMID] = self.getDMID() # DOESN'T WORK?
+        result[self.DMID] = self.dmid.getValue().getValue() # DOES WORK?! HOW?!
+        
         result[self.PLAY] = dict()
         for pid in self.players.keys():
             result[self.PLAY][pid] = self.players[pid].toJson()
+        print(result)
         return result
 
-daeolt = Player("daeolt")
+"""daeolt = Player("daeolt")
 daeolt.addEffect("short", "You laugh uncontrollably", 8.0)
 daeolt.addEffect("short", "You laugh uncontrollably", 8.0)
 daeolt.addEffect("short", "You laugh uncontrollably", 13.13)
@@ -184,8 +219,7 @@ daeolt.addEffect("perm", "You shart uncontrollably", 32.0)
 daeolt.decrementEffects(32.0)
 # print(daeolt.listEffects())
 
-debug = Server("1")
-debug.setDMID("brad")
+debug = Server("0", dict(), "brad")
 debug.addPlayer(daeolt)
 
 data = dict()
@@ -193,7 +227,7 @@ data[debug.sid] = debug
 # data["server"] = dict()
 # data["server"]["sid"] = 0
 # data["server"]["players"] = dict()
-# data["server"]["players"]["daeolt"] = daeolt.toJson()
+# data["server"]["players"]["daeolt"] = daeolt.toJson()"""
 
 """data = {
     "server":{
@@ -224,19 +258,77 @@ data[debug.sid] = debug
         }
     }
 }"""
-for sid in data.keys():
+
+# (1/4) Read JSON files
+def loadServers(basePath:str):
+    files = []
+    # servers = dict()
+    # basePath = os.path.join(config.wd, config.jsonDir)
+    with os.scandir(basePath) as iter:
+        for file in iter:
+            fileIsValid = False
+            if ".json" in file.name and "insanity-data" in file.name:
+                with open(file, config.readM) as infile:
+                    files.append(json.load(infile))
+    return files
+    # return servers
+
+# (2/4) Convert the read JSON files to Servers
+def convertFilesToObjects(files:list):
+    servers = dict()
+    for i in range(0, len(files)):
+        try:
+            p = files[i]["players"]
+            players = dict()
+            for key in p.keys():
+                curr = p[key]
+                temp = Player(key,
+                            curr["short"],
+                            curr["long"],
+                            curr["indef"],
+                            curr["perm"]
+                            )
+                curr = temp
+                players[key] = curr
+            # files[i] = data.Server(files[i]["sid"], players, files[i]["dmid"])
+            sid = files[i]["sid"]
+            print(f"DMID: {files[i]['dmid']}")
+            servers[sid] = Server(files[i]["sid"], players, DMID(files[i]["dmid"]))
+        except Exception as e:
+            print(e)
+    return servers
+
+# discord.py does all the heavy lifting in step (3/4) in main
+
+# (4/4) Helper for saveAllServers; also a good option for incremental saving later
+def saveServer(sid:str, server:Server):
     try:
+        print(f"Saving {sid}-{config.jsonFile} ...")
+        with open(os.path.join(config.wd, config.jsonDir, f"{sid}-{config.jsonFile}"), config.writeM) as outfile:
+            # server = data[sid] # Deprecated
+            jsonData = json.dumps(server.toJson(), indent=2)
+            outfile.write(jsonData)
+        print(f"Saved {sid}-{config.jsonFile} !")
+    except Exception as e:
+            print(f"Exception occurred while writing to {sid}-{config.jsonFile} !")
+            print(e)
+    return
+
+# (4/4) Write servers to JSON files
+def saveAllServers(data:dict):
+    for sid in data.keys():
+        """try:"""
         print(sid)
         if sid != None:
-            print(f"Saving {sid}-{config.jsonFile} ...")
-            with open(os.path.join(config.wd, config.jsonDir, f"{sid}-{config.jsonFile}"), config.writeM) as outfile:
-                server = data[sid]
-                jsonData = json.dumps(server.toJson(), indent=2)
-                outfile.write(jsonData)
-            print(f"Saved {sid}-{config.jsonFile} !")
-        else:
-            raise Exception(f"Could not get server id for server {sid}")
-    except Exception as e:
-        print(f"Exception occurred while writing to {sid}-{config.jsonFile} !")
-        print(e)
-print(f"Done")
+            saveServer(sid, data[sid])
+            """Deprecated
+            # print(f"Saving {sid}-{config.jsonFile} ...")
+            # with open(os.path.join(config.wd, config.jsonDir, f"{sid}-{config.jsonFile}"), config.writeM) as outfile:
+            #     server = data[sid]
+            #     jsonData = json.dumps(server.toJson(), indent=2)
+            #     outfile.write(jsonData)
+            # print(f"Saved {sid}-{config.jsonFile} !")"""
+        """else:
+            raise Exception(f"Could not get server id for server {sid}")"""
+    print(f"Done")
+    return
